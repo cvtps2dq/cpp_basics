5f2ad4630bdaf4d5909194171ed227b772783dda
Alexey Shaforostov <67270862+cvtps2dq@users.noreply.github.com>
2024-01-04 02:21:27 +0300

    what the actual fuck.
    pointers.cpp is really hard to understand lmao

--- general/pointers.cpp
+++ general/pointers.cpp
@@ -2,7 +2,14 @@
 // Created by ggkos on 1/4/2024.
 //
 #include <iostream>
-
+inline void dereference(int &big_array){
+    std::cout << "We didn't get any compile error!" << std::endl;
+    std::cout << "Dereference: " << big_array << std::endl;
+}
+inline void reference(int* big_array){
+    std::cout << "We didn't get any compile error!" << std::endl;
+    std::cout << "Reference: " << big_array << std::endl;
+}
 inline int pointers_showcase(){
     // pointers are variables, that store addresses of other variables
     // for example
@@ -19,6 +26,25 @@ inline int pointers_showcase(){
     std::cout << "Variable value: " << var << std::endl;
     std::cout << "Variable address: " << var_addr << std::endl;
     std::cout << "Variable value by address: " << *var_addr << std::endl;
+
+    // pointers needed for:
+    // 1. dynamic allocation
+    //    please see c-style-memory/cpp-style-memory
+    // 2. passing big chunks of data
+    //    our callstack is not that big.
+    //    imagine passing something very big into tiny hole.
+    //    using pointers or references, you can pass
+    //    any data type, any data in functions with ease
+    //    because pointers and references are single integer values
+    //    that can be passed through callstack
+    //    and fun fact: arrays cannot be passed in c++ / c at all :)
+
+    // demo time!!!
+
+    int* memory = new int[4096];
+    dereference( *memory);
+    reference(memory);
+    delete[] memory;
 
 
 }
